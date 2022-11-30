b132edc86bd32d277d658c01ba8cf653ff015aa2
Tomas Magnin <tomas.magnin@claro.com.ar>
2022-11-30 13:56:38 -0300

    Mitad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+/* ---------- FIREBASE ---------- */
+
+/* 1- Ir al https://console.firebase.google.com/ y cremos el proyecto */
+/* 2- Una Vez en el Panel, vamos a Compilacion > Firestore Database */
+/* 3- Usamos la segunda Opcion, Comenzar en modo de Prueba */
+/* 4- Luego en Iniciar la Coleccion, seria el equivalente al array con nuestos produsctos */
+/* 5- En Productos Añadimos los productos, Tener cuidado con los campos tipo de datos, poner a todos los productos por igual, ya sea number, string, etc.  */
+/* 6- La integracion con la App, vamos al engranaje, em la parte superior izquierda, Configuracion de Proyecto > Tus Apps (abajo del todo ), ACa seleccionamos el que es para JS, Luego colocamos el Nombre el mismo que el proyecto */
+/* 7- Luego del paso anterior, dos cosas importantes, 1era, intalar modulo (npm install firebase), 2do copiar  el string de conexion */
+
+
+
+
+/* Vamos a Crear en Firebase una Parte para productos y otra para los Pedidos  */
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -4,11 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/* Copiamos el String de Conexion de Firebase,  */
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhkO8aohvJFDccYsP6Y-gH5XS5OV1Su40",
+  authDomain: "bikeonline00.firebaseapp.com",
+  projectId: "bikeonline00",
+  storageBucket: "bikeonline00.appspot.com",
+  messagingSenderId: "1045404655778",
+  appId: "1:1045404655778:web:b8400498f7adc972eb77fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ // <React.StrictMode>
     <App />
-  </React.StrictMode>
+ // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

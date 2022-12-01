9d0d757d829fe8ce4a77c6adafb00eed734086e6
TomasMagnin <tomasmagnin@hotmail.com>
2022-12-01 00:34:41 -0300

    No me Andaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import { NavLink ,Link, useParams, Form } from "react-router-dom";
-import "./ItemListContainer";
 import { producsBK } from "./data.js";
 import ItemList from "./ItemList";
-import {getFirestore, collection, getDocs} from "firebase/firestore";
-
+import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
+                                                                
 // Para uysar el context, sacamos el darkMoe y setDarkmode, que es lo que recive.
 
-export default function ItemListContainer({}) {        
+export default function ItemListContainer() {        
 
-  console.log("Rendfer de Item List Container");
+  console.log("Render de Item List Container");
 
 
   const { idcategory } = useParams();                   /*  Sacamos el ID de categoria de la URL */
@@ -17,16 +16,20 @@ export default function ItemListContainer({}) {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {           
+
     /* Vamos a llamar a la base de Datos, y traerlos a todos usando este estado ! */
     const db = getFirestore();
-    const productos = collection(db, 'productos')
+    const productos = collection(db, "productos");
     
-    getDocs(productos).then((res) => {
-      console.log(res);
+    getDocs(productos).then((res) => {            /* Seria una promesa, y usamos el then del response y le pedimos toda la coleccion de productos */
+        console.log(res);                           /* Cuando llega le hacemos un LOG del response con los productops */
     });      
     }, [idcategory]);        /* Si cambia el ID de Categoria se ejecuta de nuevo el componente, por ende la promesa se ejecuta */
 
+
+
+
   return (
-    <div  className="text-center"  style={{ border: "2px solid red"}}></div>
-  )
-};
+    <div style = {{ border: "2px solid red", margin: `10px`}}></div>
+  );
+}
